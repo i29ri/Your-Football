96fd2f2709e47ff943ab0c6b31d3a0029c5b522e
i29ri <69230727+i29ri@users.noreply.github.com>
2021-01-25 00:44:09 +0900

    Update homes_controller.rb

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,5 @@
 class HomesController < ApplicationController
   def top
-<<<<<<< HEAD
     reviews = Review.all
     previews = Preview.all
 
@@ -8,10 +7,6 @@ class HomesController < ApplicationController
     @timelines = reviews | previews
     # 作成降順に並び替え
     @timelines.sort!{ |a, b| b.created_at <=> a.created_at }
-=======
-    @reviews = Review.all
-    @previews = Preview.all
->>>>>>> 231ee6b9a9677c622bfe76bff6b1f281a789c82f
   end
 
   def about
