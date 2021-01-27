1422d440f7c73c9500d7d2d5b061bba363e76e0c
i29ri <g1g1yuto@gmail.com>
2021-01-27 19:50:26 +0000

    -timeline-fix5

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,13 +1,18 @@
 class HomesController < ApplicationController
   def top
+    @user = User.find(current_user.id)
+    #フォローしているユーザーを取得
+    @users = @user.followings
+
     reviews = Review.all
     previews = Preview.all
-
     # それぞれの複数インスタンスを1つの配列にする
     @timelines = reviews | previews
     # 作成降順に並び替え
     @timelines.sort!{ |a, b| b.created_at <=> a.created_at }
-    
+
+    #フォローユーザーのツイートを表示
+    # @timelines = @timelines.where(user_id: @follow_users).order("created_at DESC")
   end
 
   def about
