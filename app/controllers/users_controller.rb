53370888dfe3afe2d75a6eff8f7a009f32e48e57
i29ri <g1g1yuto@gmail.com>
2021-02-10 05:40:58 +0000

    admin-fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :withdraw]
-  #before_action :check_guest, only: [:update, :withdraw]
+  #before_action :check_guest, only: [:update, :withdraw] aa
 
   def index
     @users = User.all
