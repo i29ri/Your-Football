dc3302d6ecf986394f18bd8ffba019109ebeae28
i29ri <g1g1yuto@gmail.com>
2021-02-28 12:15:11 +0000

    csv-add

---
+++ db/fixtures/production/04_user.rb
@@ -0,0 +1,12 @@
+csv = CSV.read('db/fixtures/production/users_index.csv')
+csv.each do |user|
+  user.seed do |s|
+  s.id = user[0]
+  s.email = user[1]
+  s.encrypted_password = user[2]
+  s.yourfoot_ID = user[3]
+  s.nickname = user[4]
+  s.introduction = user[5]
+  s.admin_flg = user[6]
+  end
+end
--- spec/factories/match.rb
+++ spec/factories/match.rb
@@ -1,10 +1,6 @@
 FactoryBot.define do
-  factory :user do
+  factory :match do
     id { Faker::Number.number(digits:2) }
-    yourfoot_ID { Faker::Food.fruits }
-    nickname { Faker::Name.name }
-    email { Faker::Internet.email('rspec') }
-    encrypted_password { Faker::Number.number(digits:10) }
     created_at { Faker::Date.between(from: '2021-01-01', to: '2021-02-25')}
     updated_at { Faker::Date.between(from: '2021-01-01', to: '2021-02-25')}
   end
